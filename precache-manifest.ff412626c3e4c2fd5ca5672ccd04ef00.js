955c1417f1ff39876ee53253e1934890af4bac64
Me <Measda@gmail.com>
2019-05-25 19:57:58 -0700

    Add All

---
+++ precache-manifest.ff412626c3e4c2fd5ca5672ccd04ef00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9894877fdf07bb4346a0d7656b26bb7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85d381c6833dc77282cb",
+    "url": "/static/css/main.cbad3c80.chunk.css"
+  },
+  {
+    "revision": "12dc836a1a62a2799295",
+    "url": "/static/js/2.5634b818.chunk.js"
+  },
+  {
+    "revision": "85d381c6833dc77282cb",
+    "url": "/static/js/main.b058e9d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
